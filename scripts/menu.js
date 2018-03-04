2fa2a6f1b22ccf5a5457b68e91952b10aa28038c
Charlyhm79 <charly_hm_@hotmail.com>
2018-03-04 23:47:26 +0100

    Areglo Index y smothScrolll
    Areglo Index y smothScrolll

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,7 +1,67 @@
 
 var navbarItems = document.getElementsByClassName("navbar__item");
 
-for(var i=0; i <navbarItems.length; i++);
-navbarItems[i].addEventListener("click" , function(event){
-    console.log(event);
+for(var i=0; i <navbarItems.length; i++) {
+navbarItems[i].addEventListener('click' , function(event) {
+   var goTo = this.getElementsByTagName('a')[0].href.split("#");
+   
+   deleteActiveClass();
+
+   this.classList.add("navbar__item--active");
+
+   if (goTo.length === 2){
+       event.preventDefault();
+       var sectionToGo = goTo[goTo.length - 1];
+       var elementToGo = scrollToElement(sectionToGo);
+       ScrollToElement(elementToGo);
+       
+   }
+
+
 });
+
+}
+
+
+function deleteActiveClass () {
+    for(var i=0; i <navbarItems.length; i++) {
+        navbarItems[i].classList.remove("navbar__item--active");
+
+
+    }
+
+}
+
+function scrollToElement(id){
+    var elm;
+    if (id === ''){
+
+        elm = document.getElementsByClassName("header")[0];
+    } else{
+        elm = document.getElementById(id);
+    }
+
+    return elm;
+}
+
+function ScrollToElement(element){
+
+    var jump = (element.getBoundingClientRect().top * 0.3);
+    document.body.scrollTop += jump;
+    document.documentElement.scrollTop += jump;
+
+
+   if (!element.lastJump || element.lastJump > MAth.abs(jump)){
+    element.lastJump = Math.abs(jump);
+    setTimeout(function() {
+
+        scrollToElement(element);
+
+    },25);
+
+   } else {
+       element.lastJump = null;
+
+   }
+   
+}
